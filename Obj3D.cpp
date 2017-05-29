da1c9a7db579cbc507ffa8c3015377c2f953213f
Christopher Heiden <christopherheiden9@googlemail.com>
2017-05-29 13:11:02 +0200

    first add; changed the project name; created a house; some movements of the camera and click events

---
+++ Obj3D.cpp
@@ -0,0 +1,52 @@
+#include "Obj3D.h"
+
+Obj3D::Obj3D(const char *name)
+{
+	bool res = loadOBJ(name, vertices, uvs, normals);
+
+	glGenVertexArrays(1, &VertexArrayIDTeapot);
+	glBindVertexArray(VertexArrayIDTeapot);
+
+	glGenBuffers(1, &vertexbuffer); // Kennung erhalten
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer); // Daten zur Kennung definieren
+												 // Buffer zugreifbar für die Shader machen
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0); // siehe layout im vertex shader: location = 0 
+	glVertexAttribPointer(0,  // location = 0 
+		3,  // Datenformat vec3: 3 floats fuer xyz 
+		GL_FLOAT,
+		GL_FALSE, // Fixedpoint data normalisieren ?
+		0, // Eckpunkte direkt hintereinander gespeichert
+		(void*)0); // abweichender Datenanfang ? 
+
+				   /* hineinladen des Shaders */
+	glGenBuffers(1, &normalbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(2); // siehe layout im vertex shader 
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	glGenBuffers(1, &uvbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1); // siehe layout im vertex shader 
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+}
+
+
+Obj3D::~Obj3D()
+{
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &normalbuffer);
+	glDeleteProgram(id);
+	glDeleteBuffers(1, &uvbuffer);
+}
+
+void Obj3D::draw()
+{
+	glBindVertexArray(VertexArrayIDTeapot);
+	glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+}
